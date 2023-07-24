0ffa60fb4056039d6671e8ade0d3b874a9dadebf
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-07-24 10:41:32 +0000

    execise 18 done

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,1 +1,17 @@
 # Your code here!!
+def sing():
+    my_lyrics = "";
+    for i in range(11):
+        if i == 4:
+            my_lyrics += 'whisper words of wisdom, '
+        elif i == 10: 
+            my_lyrics += 'there will be an answer, let it be'
+        else:
+            my_lyrics += 'let it be, '
+    
+    return my_lyrics;
+
+print(sing())
+
+# expected result
+#"let it be, let it be, let it be, let it be, whisper words of wisdom, let it be, let it be, let it be, let it be, let it be, there will be an answer, let it be"
